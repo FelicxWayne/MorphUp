cfcb7e7078bb5f06e5d7ed665ebed7ba9fd7bced
FelicxWayne <amalanirmalfelicx@gmail.com>
2026-03-26 18:28:15 +0530

    Initial Setup

---
+++ src/main/java/com/morphup/MorphUpApplication.java
@@ -0,0 +1,13 @@
+package com.morphup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorphUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorphUpApplication.class, args);
+	}
+
+}
